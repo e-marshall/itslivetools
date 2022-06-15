f884b621f6e7e601c11991d83b4d38d5cbb0d3fd
Emma Marshall <ekm328@gmail.com>
2022-06-15 16:03:57 -0600

    edited __init__.py

--- itslivetools/__init__.py
+++ itslivetools/__init__.py
@@ -1,4 +1,6 @@
-import xarray as xr
+from .itslivetools.py import read_in_s3
+from .itslivetools.py import find_granules_by_zone
+from .itslivetools.py import find_granule_by_point
+from .itslivetools.py import get_bbox_single
+from .itslivetools.py import get_bbox_group
 
-def xarray_function():
-    return xr.show_versions()
