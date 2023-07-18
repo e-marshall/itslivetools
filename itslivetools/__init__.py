63ced32a3bc9b978be6b113f66b0d8d1b4a0e59b
e-marshall <ekm328@gmail.com>
2023-07-18 15:42:42 -0600

    reorganize fns, add  documentation/play around  w nbdev

--- itslivetools/__init__.py
+++ itslivetools/__init__.py
@@ -1,9 +1,1 @@
-from .utility_fns import read_in_s3
-from .utility_fns import find_granules_by_zone
-from .utility_fns import find_granule_by_point
-from .utility_fns import get_bbox_single
-from .utility_fns import get_bbox_group
-from .utility_fns import get_bbox
-
-from .itslive_setup import IndGlacier
-from .itslive_setup import ind_glacier_data_prep
+__version__ = "0.0.1"
